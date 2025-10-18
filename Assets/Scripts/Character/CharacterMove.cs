d898716d41fef4cc6e5eaaeafe8e9bf9837368c6
raidnew <raidnew@gmail.com>
2025-10-18 10:49:16 +0300

    *Add free look and follow cam view

--- Assets/Scripts/Character/CharacterMove.cs
+++ Assets/Scripts/Character/CharacterMove.cs
@@ -1,3 +1,5 @@
+using Cinemachine;
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +7,9 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour, IMover, IJumper
 {
     [SerializeField] private Rigidbody _characterRb;
+    [SerializeField] private CinemachineFreeLook _camera;
+
+    private Vector3 _forwardVector;
 
     public void Jump()
     {
@@ -13,7 +18,18 @@ public class CharacterMove : MonoBehaviour, IMover, IJumper
 
     public void Move(Vector2 direction)
     {
-        _characterRb.velocity = new Vector3(direction.x * 5f, _characterRb.velocity.y, direction.y * 5f);
+        //_forwardVector.IsUnifor
+
+        _characterRb.velocity = transform.forward * direction.y + transform.right * direction.x;
+        //_characterRb.velocity = new Vector3(direction.x * 5f, _characterRb.velocity.y, direction.y * 5f);
+    }
+
+    private void FixedUpdate()
+    {
+        _forwardVector = transform.position - _camera.transform.position;
+        _forwardVector.y = 0;
+        _forwardVector.Normalize();
+        transform.forward = _forwardVector;
     }
 
 }
