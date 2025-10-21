4b0ad86e1c9b58835fdc68dc6991a70c69c2afba
raidnew <raidnew@gmail.com>
2025-10-21 09:44:03 +0300

    *hide cursor

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
